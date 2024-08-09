8912d77fef256e0e56c94c5060a062c30c2bed5b
sujitmeshram <sujitmeshramofficial@gmail.com>
2024-08-09 20:32:18 +0530

    Add project files.

---
+++ Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+
+    /* using Dto because I dont want to pass all of my Coupon models.
+     
+    we use Dto to send only those details which are required for frotnend side
+     */
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
