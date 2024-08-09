fd8556c981b1f5c5f4e7ba651f3ed4e2da02b16f
sujitmeshram <sujitmeshramofficial@gmail.com>
2024-08-09 23:32:24 +0530

    add RequestDto and ResponseDto in Mango.Web, configured base service

---
+++ Mango.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    //for sending the requests to APIs
+    public interface IBaseService
+    {
+         Task<ResponseDto?>  SendAsync(RequestDto requestDto);   
+    }
+}
