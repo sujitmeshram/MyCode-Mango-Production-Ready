829e2de29891b85a3bcab5689dc013c1f617e946
sujitmeshram <sujitmeshramofficial@gmail.com>
2024-08-11 22:26:19 +0530

    add login and registration DTO

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ Mango.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Models
+{
+    //to add more custom fields for identity, like our own fields
+    public class ApplicationUser:IdentityUser
+    {
+        public string Name { get; set; }    
+    }
+}
